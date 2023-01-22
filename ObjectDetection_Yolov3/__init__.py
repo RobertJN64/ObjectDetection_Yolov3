5f11e169d4b82d388545ddb7055ad0edbf0739a7
RobertJN64 <robertjnies@gmail.com>
2023-01-21 22:55:21 -0500

    Bounding Boxes

--- ObjectDetection_Yolov3/__init__.py
+++ ObjectDetection_Yolov3/__init__.py
@@ -13,3 +13,10 @@ def find_objects(image, thresh: float = 0.3):
     boxes, confs, class_ids = cv_api.get_box_dimensions(outputs, thresh=thresh)
     v_indexes = cv2.dnn.NMSBoxes(boxes, confs, thresh, thresh) #Filter by seperated boxes
     return [classes[class_id] for index, class_id in enumerate(class_ids) if index in v_indexes]
+
+def get_bounding_boxes(image, thresh: float = 0.3):
+    outputs = cv_api.detect_objects(image, net, output_layers)
+    boxes, confs, class_ids = cv_api.get_box_dimensions(outputs, thresh=thresh)
+    v_indexes = cv2.dnn.NMSBoxes(boxes, confs, thresh, thresh)  # Filter by seperated boxes
+    return [(classes[class_id], box) for index, (class_id, box)
+            in enumerate(zip(class_ids, boxes)) if index in v_indexes]
--- cv_test.py
+++ cv_test.py
@@ -3,3 +3,4 @@ import cv2
 
 img = cv2.imread("Sample Images/sample_001.png")
 print(ODY3.find_objects(img))
+print(ODY3.get_bounding_boxes(img))
